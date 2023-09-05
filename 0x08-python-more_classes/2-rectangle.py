929dcead4d13143cb55a4a5532f8f2ad88d8b681
Sir-bastian <sebamadziwanzira@gmail.com>
2023-09-05 05:56:56 -0700

    Area and perimeeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -21,11 +21,12 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """setter method for private instance attribute width"""
+
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         self.__width = value
-    if type(value) is not int:
-        raise TypeError("width must be an integer")
-    if value < 0:
-        raise ValueError("width must be >= 0")
 
     @property
     def height(self):
@@ -33,19 +34,27 @@ class Rectangle:
         return self.__height
 
     @height.setter
-    """setter for private instance attribute height"""
     def height(self, value):
+        """
+        setter definition for private instance attribute
+        height
+        """
+
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
         self.__height = value
-    if type(value) is not int:
-        raise TypeError("height must be an integer")
-    if value < 0:
-        raise ValueError("height must be >= 0")
 
     def area(self):
         """public instance for area calculation"""
-        return self.width * self.height
+        area = self.width * self.height
+        return area
 
     def perimeter(self):
-        perimeter = 2 * self.width + self.height
-        if width or height ==  0:
+        perimeter =  (self.width + self.height) * 2
+        if self.width == 0:
             perimeter = 0
+        if self.height == 0:
+            perimeter = 0
+        return perimeter
