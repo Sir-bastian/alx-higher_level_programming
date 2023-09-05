9fa66053adc36f693605c005d1e122764213347c
Sir-bastian <sebamadziwanzira@gmail.com>
2023-09-05 06:19:25 -0700

    String Rep

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -60,7 +60,7 @@ class Rectangle:
         return perimeter
 
     def __str__(self):
-         """returns printable string representation of the rectangle"""
+        """returns printable string representation of the rectangle"""
         string = ""
         if self.__width != 0 and self.__height != 0:
             string += "\n".join("#" * self.__width
