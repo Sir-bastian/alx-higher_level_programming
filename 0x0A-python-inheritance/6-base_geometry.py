e8321e0e4567a47494b3371ea3939d0dc928b871
Sir-bastian <sebamadziwanzira@gmail.com>
2023-09-11 11:56:24 -0700

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+
+"""Module with an empty cclass BaseGeometry"""
+
+
+class BaseGeometry:
+    """definition of class 'BaseGeometry' """
+    def __init__(self):
+        """Class initialization"""
+
+    def area(self):
+        """definition of instance method 'area' """
+        raise Exception("area() is not implemented")
