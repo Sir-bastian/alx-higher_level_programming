2bf21afca1793b3a2b85a5f097e6f88af871beba
Sir-bastian <sebamadziwanzira@gmail.com>
2023-11-14 01:36:05 -0800

    3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// A script that prints 3 lines.
+
+const lines = 'C is fun';
+const line2 = 'Python is cool';
+const line3 = 'JavaScript is Amazing';
+
+console.log(lines);
+console.log(line2);
+console.log(line3);
