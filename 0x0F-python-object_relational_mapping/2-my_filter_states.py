8f75be372edd726b3a817ec842c7d8f667a35b7c
Sir-bastian <sebamadziwanzira@gmail.com>
2023-11-18 07:26:26 -0800

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-"""
-0-select_states module
-"""
+
+""" 2-my_filter_states.py module """
+
+
 import MySQLdb
 import sys
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3]
-                         searched_name=sys.argv[4])
+if __name__ =='__main__':
+    db = MySQL.connect("""host='localhost', port=3306,
+                    user=sys.argv[1], passwd=sys.argv[2],
+                    databasename=sys.argv[3]""")
 
-    cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name LIkE
-                BINARY '{}' ORDER BY id ASC""".format(searched_name))
-    rows = cur.fetchall()
-    for row in rows:
+    cursor = db.cursor()
+    cursor.execute(""" SELECT * FROM states WHERE name LIKE BINARY
+                '{}' BY ORDER states.id ASC""".format(sys.argv[4]))
+
+    rows = cursor.fetchall()
+    for state in rows:
         print(row)
-    cur.close()
+
+    cursor.close()
     db.close()
