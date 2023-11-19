4eff7c86bb721040a107a4f7d479a6615e090ef5
Sir-bastian <sebamadziwanzira@gmail.com>
2023-11-19 02:58:31 -0800

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,19 +7,19 @@ state"""
 import MySQLdb
 import sys
 if __name__ == '__main__':
-    db = MySQLdb.connect(host=localhost, port=3306, user=sys.argv[1],
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     state_name = sys.argv[4]
 
     query = f"""SELECT cities.name FROM cities
-                JOIN states ON cities.state_id = state.id
-                WHERE states.name = {state_name}
+                JOIN states ON cities.state_id = states.id
+                WHERE states.name = '{state_name}'
                 ORDER BY cities.id ASC"""
-    cursor.execute(query, (state_name,))
+    cursor.execute(query)
     result = cursor.fetchall()
 
-    for row in rows:
+    for row in result:
         print(row)
 
     cursor.close()
