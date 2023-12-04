e32911387261389fc276f49188551c8d1f3db2b0
Sir-bastian <125459707+Sir-bastian@users.noreply.github.com>
2023-12-04 11:01:07 -0800

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/10-factorial.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-function factorial (num) {
-  if (!num || num < 2) {
-    return 1;
-  }
-  return num * factorial(num - 1);
-}
-
-console.log(factorial(parseInt(process.argv[2])));
