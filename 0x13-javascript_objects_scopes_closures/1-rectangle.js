8029c5ecf9fac24aa994b4b9453dc4950e84a6ab
Sir-bastian <125459707+Sir-bastian@users.noreply.github.com>
2023-12-04 11:01:26 -0800

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-// A class Rectangle that defines a Rectangle
-class Rectangle {
-  constructor (w, h) {
-    this.width = w;
-    this.height = h;
-  }
-}
-module.exports = Rectangle;
