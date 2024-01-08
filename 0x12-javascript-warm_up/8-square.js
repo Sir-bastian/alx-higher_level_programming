e9525cff4b855f4b87bbc8e7b3e27db57f0b2efe
Sir-bastian <sebamadziwanzira@gmail.com>
2024-01-08 12:43:38 -0800

    Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const argument = process.argv[2];
+const parsedInt = parseInt(argument);
+const character = 'X';
+const result = character.repeat(parsedInt);
+
+if (Number.isInteger(parsedInt)) {
+	for (let i = 0; i < parsedInt; i++) {
+		console.log(result);
+	}
+} else {
+	console.log('Missing size');
+}
