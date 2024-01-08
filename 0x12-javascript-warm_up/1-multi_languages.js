bab0a66910f1da8d3b7bfc55ad0392eb23adbea8
Sir-bastian <sebamadziwanzira@gmail.com>
2024-01-08 02:54:24 -0800

    3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,9 @@
 #!/usr/bin/node
 
-console.log('C is fun\nPython is cool\nJavaScript is amzing')
+const line_1 = 'C is fun';
+const line_2 = 'Python is cool';
+const line_3 = 'JavaScript is amazing';
+
+console.log(line_1);
+console.log(line_2);
+console.log(line_3);
