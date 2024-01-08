8325dacf6fa6c9075053a62a4b7ecf4a16849888
Sir-bastian <sebamadziwanzira@gmail.com>
2024-01-08 04:44:53 -0800

    Value of my argument

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-let args = process.argv.length - 2;
+const args = process.argv.length - 2;
 if (args === 0) {
-	console.log('No argument');
-}
-else if (args === 1) {
-	console.log('Argument found');
-}
-else {
-	console.log('Arguments found');
+  console.log('No argument');
+} else if (args === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-let argument = process.argv[2];
+const argument = process.argv[2];
 if (argument === undefined) {
-	console.log('No argument');
+  console.log('No argument');
 } else {
-	console.log(argument);
+  console.log(argument);
 }
