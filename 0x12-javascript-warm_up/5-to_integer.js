cef5065982aea14199839a7bb0e4caebf11d530f
Sir-bastian <sebamadziwanzira@gmail.com>
2024-01-08 06:31:58 -0800

    An integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,7 +4,7 @@ const argument = process.argv[2];
 const parsed_int = parseInt(argument);
 
 if (Number.isInteger(parsed_int)) {
-	console.log('My number: ' + parsed_int);
+  console.log('My number: ' + parsed_int);
 } else {
-	console.log('Not a number');
+  console.log('Not a number');
 }
