46300e7cff37238e5480fb4db84cedb3e94493f2
Sir-bastian <sebamadziwanzira@gmail.com>
2024-01-25 12:51:15 -0800

    cURL headers

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #A script that takes a URL and displays all HTTP methods the server will accept.
-curl -sI "$1"
+curl -sI "$1" | grep "Allow"| cut -d " " -f 2-
---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#A script that sends GET request to URL & display the body of the response.
+curl -s "$1" -H "X-School-User-Id: 98"
