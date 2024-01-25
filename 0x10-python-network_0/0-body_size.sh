7242cb4dae1606943ba2460f87cf8ab00bacab6c
Sir-bastian <sebamadziwanzira@gmail.com>
2024-01-25 12:27:20 -0800

    cURL to the end

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that takes in a URL,and dsplay the size of the body of the response.
-echo curl -sl "$1" | wc -c
+curl -sl "$1" | wc -c
---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Display the body of the response of a given URL for 200 status code  response
+curl -sL "$1"
