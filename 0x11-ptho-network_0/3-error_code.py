31df0ff6e8c621380010f18c53c8f05db5fb41c2
Sir-bastian <sebamadziwanzira@gmail.com>
2024-01-26 12:43:41 -0800

    what's my status

---
+++ 0x11-ptho-network_0/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''Captures HTTPError'''
+import urllib.request as url
+import urllib.error as urlerr
+
+
+try:
+    req = url.Request(sys.argv[1])
+    try:
+        with url.urlopen(req) as response:
+            print(response.read().decode('utf-8'))
+    except urlerr.HTTPError as h:
+        print("Error code:", h.code)
+except BaseException:
+    pass
