dfbaec5a52d815d77ef574b8581bd23c60f138f1
Sir-bastian <sebamadziwanzira@gmail.com>
2024-02-20 12:48:23 -0800

    completed tasks

--- 0x14-javascript-web_scraping/writeme.js
+++ 0x14-javascript-web_scraping/writeme.js
@@ -3,14 +3,15 @@
 // script that reads and prints the content of a file
 
 const filepath = process.argv[2];
+const content = process.argv[3];
 
 // import the file storage module
 const fs = require('fs');
 
-fs.readFile(filepath, 'utf-8', (err, content) => {
+// use the writeFile function of the fs module
+
+fs.writeFile(filepath, content, 'utf8', (err) => {
   if (err) {
     console.log(err);
-  } else {
-    console.log(content);
   }
 });
